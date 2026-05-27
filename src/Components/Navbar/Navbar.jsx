bd4fa3db973a5671dd66a2b5bc3d885c806153e0
Md Ziaur Rahman <iziaurofficial@gmail.com>
2026-05-27 20:30:58 +0530

    Navbar made

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import "./Navbar.css"
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
+import { useState } from "react";
 
 function Navbar(){
-    return <div>
 
-    </div>
+
+    let[menu,setmenu]=useState("Shop");
+    return (
+        <div className="navbar">
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>Threadly</p>
+        </div>
+        
+        <ul className="nav-menu">
+            <li onClick={()=>{setmenu("Shop")}}>Shop {menu=="Shop"?<hr/>:null}</li>
+            <li onClick={()=>{setmenu("Men")}}> Men{menu=="Men"?<hr/>:null}</li>
+            <li onClick={()=>{setmenu("Women")}}>Women{menu=="Women"?<hr/>:null}</li>
+            <li onClick={()=>{setmenu("Kids")}}>Kids{menu=="Kids"?<hr/>:null}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon}></img>
+            <div className="nav-card-count">0</div>
+        </div>
+</div>
+        )
 
 
 
